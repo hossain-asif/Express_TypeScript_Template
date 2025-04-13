949aeab6ef83c7e7dfc856cb8ce571c921189936
hossain-asif <minhazhossainasif.com>
2025-04-13 16:33:02 +0600

    added api versioning.

---
+++ src/routers/v1/ping.router.ts
@@ -0,0 +1,10 @@
+
+import express from 'express';
+import { pingHandler } from '../../controllers/ping.controller';
+
+
+const pingRouter = express.Router();
+
+pingRouter.get('/', pingHandler);
+
+export default pingRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 
 import express from 'express';
 import { serverConfig } from './config'; 
-import pingRouter from './routers/ping.router';
+import v1Router from './routers/v1/index.router';
+import v2Router from './routers/v2/index.router';
 
 const app = express();
 
 // registering all the routers and their corresponding routes with our app server object.
-app.use(pingRouter);
+app.use('/api/v1', v1Router);
+app.use('/api/v2', v2Router);
 
 app.listen(serverConfig.PORT, ()=>{
     console.log(`Server is running on http://localhost:${serverConfig.PORT}`);
